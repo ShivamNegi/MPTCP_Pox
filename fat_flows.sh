c2ae2f463932562c3b950cf89cd368bb2df468a9
Shivam Negi <shivamnegi2019@gmail.com>
2022-04-27 14:04:29 -0400

    Modifying flow scripts

---
+++ fat_flows.sh
@@ -0,0 +1,8 @@
+python generate_cmds_fat.py 4 1 ecmp > fat_mn_script_ecmp_1flows
+python generate_cmds_fat.py 4 2 ecmp > fat_mn_script_ecmp_2flows
+python generate_cmds_fat.py 4 3 ecmp > fat_mn_script_ecmp_3flows
+python generate_cmds_fat.py 4 4 ecmp > fat_mn_script_ecmp_4flows
+python generate_cmds_fat.py 4 5 ecmp > fat_mn_script_ecmp_5flows
+python generate_cmds_fat.py 4 6 ecmp > fat_mn_script_ecmp_6flows
+python generate_cmds_fat.py 4 7 ecmp > fat_mn_script_ecmp_7flows
+python generate_cmds_fat.py 4 8 ecmp > fat_mn_script_ecmp_8flows
--- jelly_flows.sh
+++ jelly_flows.sh
@@ -1,8 +1,8 @@
-python generate_cmds_jelly.py 20 8 1 adjList_4links_20sw ecmp_4links_20sw > jelly_mn_script_ecmp_8flows1
-python generate_cmds_jelly.py 20 8 2 adjList_4links_20sw ecmp_4links_20sw > jelly_mn_script_ecmp_8flows2
-python generate_cmds_jelly.py 20 8 3 adjList_4links_20sw ecmp_4links_20sw > jelly_mn_script_ecmp_8flows3
-python generate_cmds_jelly.py 20 8 4 adjList_4links_20sw ecmp_4links_20sw > jelly_mn_script_ecmp_8flows4
-python generate_cmds_jelly.py 20 8 5 adjList_4links_20sw ecmp_4links_20sw > jelly_mn_script_ecmp_8flows5
-python generate_cmds_jelly.py 20 8 6 adjList_4links_20sw ecmp_4links_20sw > jelly_mn_script_ecmp_8flows6
-python generate_cmds_jelly.py 20 8 7 adjList_4links_20sw ecmp_4links_20sw > jelly_mn_script_ecmp_8flows7
-python generate_cmds_jelly.py 20 8 8 adjList_4links_20sw ecmp_4links_20sw > jelly_mn_script_ecmp_8flows8
+python generate_cmds_jelly.py 20 8 1 adjList_4links_20sw ecmp_4links_20sw > jelly_mn_script_ecmp_1flows
+python generate_cmds_jelly.py 20 8 2 adjList_4links_20sw ecmp_4links_20sw > jelly_mn_script_ecmp_2flows
+python generate_cmds_jelly.py 20 8 3 adjList_4links_20sw ecmp_4links_20sw > jelly_mn_script_ecmp_3flows
+python generate_cmds_jelly.py 20 8 4 adjList_4links_20sw ecmp_4links_20sw > jelly_mn_script_ecmp_4flows
+python generate_cmds_jelly.py 20 8 5 adjList_4links_20sw ecmp_4links_20sw > jelly_mn_script_ecmp_5flows
+python generate_cmds_jelly.py 20 8 6 adjList_4links_20sw ecmp_4links_20sw > jelly_mn_script_ecmp_6flows
+python generate_cmds_jelly.py 20 8 7 adjList_4links_20sw ecmp_4links_20sw > jelly_mn_script_ecmp_7flows
+python generate_cmds_jelly.py 20 8 8 adjList_4links_20sw ecmp_4links_20sw > jelly_mn_script_ecmp_8flows
